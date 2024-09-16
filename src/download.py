d1b030d49299acb83e133ccad05a5efe90ae63b7
David Zarzavilla Borrego <dzarzavillaborrego@uma.es>
2024-09-16 22:18:24 +0200

    Update download.py

--- src/download.py
+++ src/download.py
@@ -1,3 +1,4 @@
+#Opcion 1
 import yt_dlp
 import os
 
@@ -20,3 +21,18 @@ def download_audio(youtube_url):
 
 # Ejemplo de uso
 download_audio("https://www.youtube.com/watch?v=xxhET61yB1A")
+
+#Opcion 2
+from pytube import YouTube
+import os
+
+def download_audio_from_youtube(url):
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    audio_file = audio_stream.download(output_path="audio_files")
+    base, ext = os.path.splitext(audio_file)
+    new_file = base + '.mp3'
+    os.rename(audio_file, new_file)
+    return new_file
+
+audio_path = download_audio_from_youtube('https://www.youtube.com/watch?v=12345')
